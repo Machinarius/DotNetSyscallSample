d58e10c341371f298d9f5b790746601c12f2ccb6
Germán Valencia <machinarius92@gmail.com>
2016-08-05 15:56:18 -0500

    Initial commit. Working CHmod

---
+++ SyscallTest.Native/syscalls.c
@@ -0,0 +1,11 @@
+﻿#include <unistd.h>
+#include <sys/syscall.h>
+#include <errno.h>
+#include <stdio.h>
+
+int __attribute__ ((visibility ("default"))) chmodsc(char* filename, int mask) 
+{
+	int callResult;
+	callResult = syscall(SYS_chmod, filename, mask);
+	return callResult;
+}
---
+++ SyscallTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace SyscallTest
+{
+	class MainClass
+	{
+		[DllImport("SyscallTest", EntryPoint = "chmodsc")]
+		public static extern int NativeChmod(string filename, int accessMask);
+
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Input filename");
+			var filename = Console.ReadLine ();
+
+			if (!File.Exists (filename)) 
+			{
+				Console.WriteLine ("The file specified does not exist");
+				return;
+			}
+
+			Console.WriteLine ("Input access mask");
+			var accessMaskString = Console.ReadLine ();
+			var accessMask = 0;
+
+			if (!int.TryParse (accessMaskString, out accessMask)) 
+			{
+				Console.WriteLine ("Invalid access mask specified");
+				return;
+			}
+
+			var callResult = NativeChmod (filename, accessMask);
+			Console.WriteLine ("chmod result: " + callResult);
+		}
+	}
+}
